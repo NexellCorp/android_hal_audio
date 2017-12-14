dedc8a79c454d6f3c90e575f50f7b0302fa09ea6
Hyunseok Jung <hsjung@nexell.co.kr>
2017-12-14 13:59:00 +0900

    audio hw hal: change audio hal base code using nougat api
    This patch changes audio hal base code using nougat api.
    
    Change-Id: I21ac5e9d290583105506547240d985561f90906a
    Signed-off-by: Hyunseok Jung <hsjung@nexell.co.kr>

---
+++ Android.mk
@@ -0,0 +1,55 @@
+# Copyright (C) 2011 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# The default audio HAL module, which is a stub, that is loaded if no other
+# device specific modules are present. The exact load order can be seen in
+# libhardware/hardware.c
+#
+# The format of the name is audio.<type>.<hardware/etc>.so where the only
+
+ifeq ($(strip $(BOARD_USES_AUDIO_N)),true)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE := audio.primary.$(TARGET_BOOTLOADER_BOARD_NAME)
+LOCAL_SRC_FILES := \
+	audio_hw.c
+LOCAL_SHARED_LIBRARIES := \
+	liblog \
+	libcutils \
+	libtinyalsa \
+	libaudioroute \
+	libexpat
+LOCAL_C_INCLUDES += \
+	external/tinyalsa/include \
+	external/expat/lib \
+	$(call include-path-for, audio-route) \
+	$(call include-path-for, audio-effects)
+ifeq ($(strip $(BOARD_USES_NXVOICE)),true)
+LOCAL_CFLAGS += -DUSES_NXVOICE
+LOCAL_SHARED_LIBRARIES += \
+	libnxvoice \
+	libpvo \
+	libpovosource
+LOCAL_C_INCLUDES += \
+	device/nexell/library/nx-smartvoice \
+	device/nexell/library/libpowervoice
+endif
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif
